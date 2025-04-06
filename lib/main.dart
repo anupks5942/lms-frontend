61b62df1b64ec95fe5647cd1cea4fa2750ee157d
anupks5942 <anupkumarsoni@katyayaniorganics.com>
2025-04-06 18:30:10 +0530

    changed main

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:provider/provider.dart';
-import 'package:sizer/sizer.dart';
 import 'core/router/app_router.dart';
-import 'features/auth/view_model/auth_provider.dart';
 import 'utils/app_theme.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => Sizer(
-      builder:
-          (context, orientation, screenType) => ProviderScope(
-            child: MaterialApp.router(
-              routerConfig: router,
-              title: 'EduLearn',
-              debugShowCheckedModeBanner: false,
-              theme: AppTheme.light,
-              darkTheme: AppTheme.dark,
-            ),
-          ),
-    )
-  ;
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
+      routerConfig: router,
+      title: 'LMS App',
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+    );
+  }
 }
